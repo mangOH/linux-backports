e2f86721b0ca159ff1a2757bf18f7b5cde2f4042
David Frey <dfrey@sierrawireless.com>
2020-05-14 11:06:04 -0700

    remove backport of of_dma_configure()
    This function was already backported into the Sierra Wireless kernel
    either by Sierra Wireless or Qualcomm.

--- backport/backport-include/linux/of_device.h
+++ backport/backport-include/linux/of_device.h
@@ -3,11 +3,6 @@
 #include_next <linux/of_device.h>
 #include <linux/version.h>
 
-#if LINUX_VERSION_IS_LESS(4,1,0)
-static inline void of_dma_configure(struct device *dev, struct device_node *np)
-{}
-#endif /* < 4.1.0 */
-
 #if LINUX_VERSION_IS_LESS(4,12,0)
 extern ssize_t of_device_modalias(struct device *dev, char *str, ssize_t len);
 #endif
